2a15b8af898048ece0e2aa7348c3a697aff23fe7
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-03-17 16:41:40 +0200

    Created class Task with properties

---
+++ AntiAircraftComplex/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiAircraftComplex
+{
+    class Task
+    {
+        public readonly uint countOfComples;
+        public readonly uint averageArrivaleRate;
+        public readonly double[] averageServiceTimes;
+        public readonly double[] destroyProbabilities;
+
+        public Task(uint countOfComples, uint averageArrivaleRate, IList<double> averageServiceTimes, IList<double> destroyProbabilities)
+        {
+            this.countOfComples = countOfComples;
+            this.averageArrivaleRate = averageArrivaleRate;
+            this.averageServiceTimes = averageServiceTimes.ToArray();
+            this.destroyProbabilities = destroyProbabilities.ToArray();
+        }
+
+        public void Solve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
