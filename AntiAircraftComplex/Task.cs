ee60f4379b04422ca94eb58da1056eb12be0ccbd
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-03-20 09:40:25 +0200

    Changed countOfComplex (now it is a const). Added array flightProbabilities. Added method to calculate different probabilities (of cancel, of damage etc)

--- AntiAircraftComplex/Task.cs
+++ AntiAircraftComplex/Task.cs
@@ -8,42 +8,49 @@ namespace AntiAircraftComplex
 {
     class Task : ITask
     {
-        public readonly uint countOfComples;
+        public const byte COUNT_OF_COMPLEX = 2;
         public readonly uint averageArrivaleRate;
         public readonly decimal[] averageServiceTimes;
         public readonly decimal[] destroyProbabilities;
         public decimal[] alphas;
+        public (decimal, string)[] flightProbabilities;
 
         public bool IsSolved { get; set; }
-        public Task(uint countOfComples, uint averageArrivaleRate, IList<decimal> averageServiceTimes, IList<decimal> destroyProbabilities)
-        {
-            this.countOfComples = countOfComples;
+        public Task(uint averageArrivaleRate, IList<decimal> averageServiceTimes, IList<decimal> destroyProbabilities)
+        {            
             this.averageArrivaleRate = averageArrivaleRate;
-            this.averageServiceTimes = averageServiceTimes.Select(val => 60 / val).ToArray();
+            this.averageServiceTimes = averageServiceTimes.ToArray();
             this.destroyProbabilities = destroyProbabilities.ToArray();
+            flightProbabilities = new (decimal, string)[3];
         }
 
         public string GetResult()
         {
-            return "IDK";
+            return GetBestResult().ToString();
         }
-
 
         public void Solve()
         {
             alphas = CalculateAlphas();
+
         }
 
         private decimal[] CalculateAlphas()
         {
-            decimal[] array = new decimal[countOfComples];
-            for(int i = 0; i < countOfComples; ++i)
+            decimal[] array = new decimal[COUNT_OF_COMPLEX];
+            for(int i = 0; i < COUNT_OF_COMPLEX; ++i)
             {
-                array[i] = averageArrivaleRate / averageServiceTimes[i];
+                array[i] = averageArrivaleRate * averageServiceTimes[i];
             }
             return array;
         }
 
+        private void CalcucateProbabilityOfFlightWhen1Row()
+        {
+            var result = 0;
+
+            flightProbabilities[0] = (result, String.Empty);
+        }
         private decimal GetProbabilityOfFlightThrough()
         {
             return 1 - GetProbabilityOfNotFlightThrough();
@@ -51,6 +58,32 @@ namespace AntiAircraftComplex
         private decimal GetProbabilityOfNotFlightThrough()
         {
             throw new NotImplementedException();
+        }
+        private decimal CalculateProbabilityOfDamage()
+        {
+            throw new NotImplementedException();
+        }
+        private decimal CalculateProbabilityOfCancel()
+        {
+            throw new NotImplementedException();
+        }
+        private decimal GetAverageArrivalRateAtSecondRow()
+        {
+            throw new NotImplementedException();
+        }
+        private decimal CalculateTotalProbabilityForTwoRows()
+        {
+            throw new NotImplementedException();
+        }
+        private (decimal, string) GetBestResult()
+        {
+            var minimumFlightProbability = flightProbabilities[0];
+            for(int i = 1; i < flightProbabilities.Length; ++i)
+            {
+                if (flightProbabilities[i].Item1 < minimumFlightProbability.Item1)
+                    minimumFlightProbability = flightProbabilities[i];
+            }
+            return minimumFlightProbability;
         }
     }
 }
