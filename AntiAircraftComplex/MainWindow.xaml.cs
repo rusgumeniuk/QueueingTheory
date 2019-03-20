24c7f688ed20a0d3fba4a21d6abec80963e35dd6
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-03-20 09:37:42 +0200

    Removed methods for dynamic adding UIelements. Added method to parse and return average arrival rate

--- AntiAircraftComplex/MainWindow.xaml.cs
+++ AntiAircraftComplex/MainWindow.xaml.cs
@@ -10,50 +10,17 @@ namespace AntiAircraftComplex
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        public ObservableCollection<UIElement> Controls { get; set; } = new ObservableCollection<UIElement>();
-        private uint countOfComplex = 0;
+    {               
         public MainWindow()
         {
-            InitializeComponent();
-            VarGrid.DataContext = this;
-        }
-
-        private void TextBoxNumberOfComples_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (String.IsNullOrEmpty(TextBoxNumberOfComples.Text))
-                return;
-            if (!uint.TryParse(TextBoxNumberOfComples.Text, out countOfComplex))
-            {
-                MessageBox.Show("Помилка вводу!\nВведіть натуральне число.");
-                return;
-            }
-            AddNewPanels(countOfComplex);
-        }
-        private void AddNewPanels(uint count)
-        {
-            Controls.Clear();
-            for (uint i = 0; i < count; ++i)
-            {
-                AddNewUIElements(i);
-            }
-        }
-        private void AddNewUIElements(uint index)
-        {
-            Controls.Add(new Label { Content = $"Введіть середню тривалість циклу стрільби комплексу №{index + 1}:" });
-            Controls.Add(new TextBox { Name = $"TextBoxServiceTime{index + 1}" });
-            Controls.Add(new Label { Content = $"Введіть ймовірність враження літака комплексом №{index + 1}:" });
-            Controls.Add(new TextBox { Name = $"TextBoxDestroyProbability{index + 1}" });
-            Controls.Add(new Separator());
+            InitializeComponent();            
         }       
 
         private void BtnSolve_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                if (countOfComplex < 1 || !uint.TryParse(TextBoxAverageArrivalRate.Text, out uint averageArrivalRate))
-                    throw new ArgumentException("Введено хибні дані. Будь ласка спробуйте ще раз.");
-                Task task = new Task(countOfComplex, averageArrivalRate, GetAverageServiceTimes(), GetDestroyProbabilities());
+                Task task = new Task(GetAverageArrivalRate(), GetAverageServiceTimes(), GetDestroyProbabilities());
                 task.Solve();
             }
             catch (Exception ex)
@@ -64,18 +31,17 @@ namespace AntiAircraftComplex
         private IList<decimal> GetAverageServiceTimes()
         {
             IList<decimal> values = new List<decimal>();
-            for (int i = 0; i < Controls.Count; ++i)
+            for (int i = 0; i < MainGrid.Children.Count; ++i)
             {
-                if (Controls[i] is TextBox && (Controls[i] as TextBox).Name.Contains("TextBoxServiceTime"))
+                if (MainGrid.Children[i] is TextBox && (MainGrid.Children[i] as TextBox).Name.Contains("TextBoxServiceTime"))
                 {
-                    var text = (Controls[i] as TextBox).Text;
-                    if (!decimal.TryParse(text, out decimal result))
-                    {
-                        break;
+                    var text = (MainGrid.Children[i] as TextBox).Text;
+                    if (!uint.TryParse(text, out uint result))
+                    {                        
                         throw new ArgumentException($"Неправильне значення в полі введення часу обслуговування №{i + 1}");
                     }
                     else
-                        values.Add(result);
+                        values.Add((decimal)result/60);
                 }
             }
             return values;
@@ -83,11 +49,11 @@ namespace AntiAircraftComplex
         private IList<decimal> GetDestroyProbabilities()
         {
             IList<decimal> values = new List<decimal>();
-            for (int i = 0; i < Controls.Count; ++i)
+            for (int i = 0; i < MainGrid.Children.Count; ++i)
             {
-                if (Controls[i] is TextBox && (Controls[i] as TextBox).Name.Contains("TextBoxDestroyProbability"))
+                if (MainGrid.Children[i] is TextBox && (MainGrid.Children[i] as TextBox).Name.Contains("TextBoxDestroyProbability"))
                 {
-                    var text = (Controls[i] as TextBox).Text;
+                    var text = (MainGrid.Children[i] as TextBox).Text;
                     if (!decimal.TryParse(text, out decimal result))
                     {
                         throw new ArgumentException($"Неправильне значення в полі введення ймовірності збиття №{i + 1}");
@@ -98,5 +64,9 @@ namespace AntiAircraftComplex
             }
             return values;
         }        
+        private uint GetAverageArrivalRate()
+        {
+            return uint.TryParse(TextBoxAverageArrivalRate.Text, out var result) ? result : throw new ArgumentException("Невірно введена інтенсивність прольотів літаків!");
+        }
     }
 }
